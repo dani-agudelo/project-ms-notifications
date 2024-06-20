d5ab56bb5e7aafdae8eae673b1219b539f263a6e
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2024-06-20 06:14:21 -0500

    Build production.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
+from gevent.pywsgi import WSGIServer
 
 from views import app
 
 
 def main():
     load_dotenv()
-    app.run(port=5001, debug=True)
+    # app.run(port=5001, debug=True) # For development
+    WSGIServer(("", 5001), app).serve_forever()  # For production
 
 
 if __name__ == "__main__":
